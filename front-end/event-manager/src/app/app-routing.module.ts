905f302e3bd20d279c5a1ce35ed03201b22f471a
DJ-M87 <mde-jage@student.wethinkcode.co.za>
2021-05-10 17:17:14 +0200

    still working on create events

--- front-end/event-manager/src/app/app-routing.module.ts
+++ front-end/event-manager/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CreateEventsComponent} from './create-events/create-events.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create-events', component: CreateEventsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
