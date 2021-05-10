a2035408f748c7f3f158610f566c957319d49d34
DJ-M87 <mde-jage@student.wethinkcode.co.za>
2021-05-10 15:57:46 +0200

    added interfaces, create_event component, still working on it, not complete

--- front-end/event-manager/src/app/app.module.ts
+++ front-end/event-manager/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEventsComponent } from './create-events/create-events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEventsComponent
   ],
   imports: [
     BrowserModule,
