2712f9ef1f4cf98ffc15130677edfd60a53ae772
asiguqaCPT <asiguqa@student.wethinkcode.co.za>
2021-05-11 09:46:48 +0200

    created login component

--- front-end/event-manager/src/app/app.module.ts
+++ front-end/event-manager/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateEventsComponent } from './create-events/create-events.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateEventsComponent
+    CreateEventsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
