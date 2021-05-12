5012b252c1159c9b16ddedafc35b29adc2cd86b0
Bondo Kalombo <bondo@bbd.co.za>
2021-05-12 13:23:19 +0200

    Added endpoints for the events, venues, tickets, vip-tickets

--- back-end/dboperations.js
+++ back-end/dboperations.js
@@ -61,7 +61,143 @@ async function updateTicketById(ticket) {
     catch(err) { console.log(err) }
 }
 
+/*****************************************************  tblVIPTickets  ************************************/
+//Get ALL
+async function getVIPTickets() {
+    try {
+        let pool = await sql.connect(config);
+        let vipTickets = await pool.request().query("SELECT * FROM tblVIPTicket");
+        return vipTickets.recordsets;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
 
+// Get by Id
+async function getVIPTicketById(id) {
+    try {
+        let pool = await sql.connect(config);
+        let vipTicket = await pool.request()
+            .input('input_parameter', sql.Int, id)
+            .query("SELECT * FROM tblVIPTicket WHERE VIPTicketId = @input_parameter");
+            return vipTicket.recordsets;
+    }
+    catch(error) {
+       
+    }
+}
+
+// Post
+async function insertVIPTicket(ticket) {
+    
+    try {
+        let pool = await sql.connect(config);
+        let insertVIPTicket = await pool.request()
+            .input('Price', sql.Money, ticket.Price)
+            .input('NumTicketsLeft', sql.Int, ticket.NumTicketsLeft)
+            .execute('spInsertVIPTicket'); 
+        return insertVIPTicket.recordsets;
+    }
+    catch (err) { console.log(err); }
+}
+
+/***********************************     tblVIPTicket End *********************/
+
+
+/*****************************************************  tblTickets Queries  ************************************/
+//Get ALL
+async function getTickets() {
+    try {
+        let pool = await sql.connect(config);
+        let tickets = await pool.request().query("SELECT * FROM tblTicket");
+        return tickets.recordsets;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+// Get by Id
+async function getTicketById(id) {
+    try {
+        let pool = await sql.connect(config);
+        let ticket = await pool.request()
+            .input('input_parameter', sql.Int, id)
+            .query("SELECT * FROM tblTicket WHERE TicketId = @input_parameter");
+            return ticket.recordsets;
+    }
+    catch(error) {
+       
+    }
+}
+
+// Post
+async function insertTicket(ticket) {
+    
+    try {
+        let pool = await sql.connect(config);
+        let ticket = await pool.request()
+            .input('TicketType', sql.Int, ticket.TicketType)
+            .input('EventId', sql.Int, ticket.EventId)
+            .input('UserId', sql.Int, ticket.EventId)
+            .execute('spInsertTicket'); 
+        return ticket.recordsets;
+    }
+    catch (err) { console.log(err); }
+}
+
+/***********************************     tblTickets End *********************/
+
+/*****************************************************  tblVenue Queries  ************************************/
+//Get ALL
+async function getVenues() {
+    try {
+        let pool = await sql.connect(config);
+        let venue = await pool.request().query("SELECT * FROM tblVenue");
+        return venue.recordsets;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+// Get by Id
+async function getVenueById(id) {
+    try {
+        let pool = await sql.connect(config);
+        let venue = await pool.request()
+            .input('input_parameter', sql.Int, id)
+            .query("SELECT * FROM tblVenue WHERE VenueId = @input_parameter");
+            return venue.recordsets;
+    }
+    catch(error) {
+       
+    }
+}
+
+// Post
+async function insertVenue(venue) {
+    
+    try {
+        let pool = await sql.connect(config);
+        let insertVenue = await pool.request()
+            .input('Country', sql.VarChar(100), venue.Country)
+            .input('Province', sql.VarChar(50), venue.Province)
+            .input('StreetAddress', sql.VarChar(150), venue.StreetAddress)
+			.input('VenueName', sql.VarChar(50), venue.VenueName)
+			.input('City', sql.VarChar(150), venue.City)
+			.input('ZipCode', sql.VarChar(10), venue.ZipCode)
+            .execute('spInsertVenue'); 
+        return insertVenue.recordsets;
+    }
+    catch (err) { console.log(err); }
+}
+
+/***********************************     tblVenue End *********************/
+
+
+/***********************************     tblUser Queries Start *********************/
 /**
  * tblUser Queries 
  */
@@ -112,12 +248,82 @@ async function createUser(user) {
     catch (err) { console.log(err); }
 }
 
+/***************************************   End User Queries *************************************/
+
+/***********************************     tblEvent Queries Start *********************/
+/**
+ * tblUser Queries 
+ */
+// Get All
+async function getEvents() {
+    try {
+        let pool = await sql.connect(config);
+        let events = await pool.request().query("SELECT * FROM tblEvent");
+        return events.recordsets;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+// Get by Id
+async function getEventById(id) {
+    try {
+        let pool = await sql.connect(config);
+        let events = await pool.request()
+            .input('input_parameter', sql.Int, id)
+            .query("SELECT * FROM tblEvent WHERE EventId = @input_parameter");
+            return tblUsers.recordsets;
+    }
+    catch(error) {
+       
+    }
+}
+
+
+// Post
+async function insertEvent(event) {
+    
+    try {
+        console.log(event.StartTime)
+        let pool = await sql.connect(config);
+        let insertEvent = await pool.request()
+            .input('Description', sql.VarChar(450), event.Description)
+            .input('MaxNumGuests', sql.Int, event.MaxNumGuests)
+            .input('EventDate', sql.Date, event.EventDate)
+            .input('StartTime', sql.VarChar(10), event.StartTime)
+            .input('EndTime', sql.VarChar(10), event.EndTime)
+            .input('EventCreatorId', sql.Int, event.EventCreatorId)
+            .input('BannerId', sql.Int, event.BannerId)
+            .input('EventTitle', sql.VarChar(50), event.EventTitle)
+            .input('EventSubTitle', sql.VarChar(50), event.EventSubTitle)
+            .input('VenueId', sql.Int, event.VenueId)
+            .execute('spInsertEvent'); 
+        return insertEvent.recordsets;
+    }
+    catch (err) { console.log(err); }
+}
+
+/***************************************   End Event Queries *************************************/
+
 module.exports = {
+    insertEvent : insertEvent,
+    getEventById : getEventById,
+    getVenues : getVenues,
+    getVenueById : getVenueById,
     getGeneralTickets : getGeneralTickets,
     getGeneralTicketWithId : getGeneralTicketWithId,
     addGeneralTicket : addGeneralTicket,
     updateTicketById : updateTicketById,
     getUsers : getUsers,
     getUsersById : getUsersById,
-    createUser : createUser
+    createUser : createUser,
+    getVIPTickets : getVIPTickets,
+    getVIPTicketById : getVIPTicketById,
+    insertVIPTicket : insertVIPTicket,
+    getTickets : getTickets,
+    getTicketById : getTicketById,
+    insertTicket : insertTicket,
+    insertVenue : insertVenue,
+    getEvents : getEvents
 }
